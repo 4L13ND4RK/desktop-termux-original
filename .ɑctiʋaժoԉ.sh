3b48b3db717437409d4f8e3b8e8d7557d679d618
☠ Ɑℓîeɳ Ɗɑℜƙ ☠ <92875451+4L13ND4RK@users.noreply.github.com>
2021-12-15 18:37:58 -0500

    Update .ɑctiʋaժoԉ.sh

--- .ɑctiʋaժoԉ.sh
+++ .ɑctiʋaժoԉ.sh
@@ -18,13 +18,16 @@ echo " apagando los host "
 echo "listo..✅"
 echo " para iniciar usa el script start-desktop "
 killall Xvnc 
-#cp /data/data/com.termux/files/home/.vnc/xstartup /data/data/com.termux/files/home 
+cp /data/data/com.termux/files/home/.vnc/xstartup /data/data/com.termux/files/home 
 rm -rf .vnc 
 rm install-desktop.sh
 touch .ʞiℓℓ.sh
 echo "rm .ɑctiʋaժoԉ.sh" >> .ʞiℓℓ.sh
-#echo " cd $HOME " >> .ʞiℓℓ.sh
+echo " cd $HOME " >> .ʞiℓℓ.sh
 echo " rm -rf /data/data/com.termux/files/home/.vnc " >> .ʞiℓℓ.sh 
-#echo " mkdir /data/data/com.termux/files/home/.vnc " >> .ʞiℓℓ.sh
-#echo " cp /data/data/com.termux/files/home/xstartup /data/data/com.termux/files/home/.vnc " >> .ʞiℓℓ.sh
+echo " mkdir /data/data/com.termux/files/home/.vnc " >> .ʞiℓℓ.sh
+echo " cp /data/data/com.termux/files/home/xstartup /data/data/com.termux/files/home/.vnc " >> .ʞiℓℓ.sh
+echo "rm /data/data/com.termux/files/home/xstartup " >> .ʞiℓℓ.sh 
+echo " chmod 777 /data/data/com.termux/files/home/.vnc/xstartup " >> .ʞiℓℓ.sh
+chmod 777 *
 bash .ʞiℓℓ.sh
